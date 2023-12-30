77ab328a9c586e68d4315bae2f6937e47ed4772f
RelexSun <88622289+RelexSun@users.noreply.github.com>
2023-12-30 12:49:52 +0700

    🔥

--- src/lesson2.ts
+++ src/lesson2.ts
@@ -119,11 +119,56 @@ function render(document: unknown) {
 
 // ----------------------------------------------------
 // --------------- The never Type --------------------
-function processEvents(): never {
-  while (true) {
-    // Read a message from a queue
-  }
-}
+// function processEvents(): never {
+//   while (true) {
+//     // Read a message from a queue
+//   }
+// }
 
-processEvents();
-console.log("Hello World");
+// processEvents();
+// console.log("Hello World");
+
+// ----------------------------------------------------
+// --------------- Exercies --------------------
+// TODO: Given the data below, define a type alias for representing users.
+type Users = {
+  name: string;
+  age: number;
+  occupation?: string;
+};
+
+// TODO: Birds fly. Fish swim. A Pet can be a Bird or Fish. Use type aliases to represent these.
+type Bird = {
+  fly: () => void;
+};
+
+type Fish = {
+  swim: () => void;
+};
+type Pet = Bird | Fish;
+
+// TODO: Define a type for representing the days of week. Valid values are “Monday”, “Tuesday”, etc.
+type DayOfWeek =
+  | "Monday"
+  | "Tuesday"
+  | "Wednesday"
+  | "Thursday"
+  | "Friday"
+  | "Saturday"
+  | "Sunday";
+
+let users: Users[] = [
+  { name: "John Smith", age: 30, occupation: "Software engineer" },
+  { name: "Kate Müller", age: 28 },
+];
+
+// TODO: Simplify the following code snippets:
+let user = getUser();
+console.log(user?.address?.street);
+
+let x = foo ?? bar();
+
+// TODO: What is the problem in this piece of code?
+let value: unknown = "a"; // unkown type
+// Narrowing the type first
+if (typeof value === "string") console.log(value.toUpperCase());
